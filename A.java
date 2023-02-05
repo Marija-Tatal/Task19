5caec70430eed351feaa49a70a21542ed601bfd9
Marija-Tatal <79601185+Marija-Tatal@users.noreply.github.com>
2023-02-05 19:03:14 +0100

    Add files via upload

---
+++ A.java
@@ -0,0 +1,27 @@
+package Class19HW;
+
+public class A {
+    //   Write program to inherit class A that has method printF which is static and call or reuse that method into class B
+
+    static void printF(){
+        System.out.println("Calling method F form class A");
+    }
+
+
+}
+class B extends A {
+    void printInfo() {
+        System.out.println("Hello Java");
+
+    }
+
+
+    public static void main(String[] args) {
+
+        B classB = new B();
+        classB.printInfo();
+        classB.printF();
+
+
+    }
+}
